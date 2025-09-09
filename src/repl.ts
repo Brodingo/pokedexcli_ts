02253c96df97eb58afe6f06283b636105ed58863
Brodingo <brodyk@gmail.com>
2025-09-08 21:37:48 -0400

    test setup

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,4 @@
 export function cleanInput(input: string): string[] {
     // Split the input by spaces and filter out any empty strings
-    return input.split(' ').filter(arg => arg.trim() !== '');
+    return input.toLowerCase().split(' ').filter(arg => arg.trim() !== '');
 }
